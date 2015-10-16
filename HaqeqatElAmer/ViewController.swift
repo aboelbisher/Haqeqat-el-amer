e5a7921434a04bebda856e8869d05fc52de303ec
muhammad abed el razek <aboelbisher.176@gmail.com>
2015-10-16 13:14:51 +0300

    Initial Commit

---
+++ HaqeqatElAmer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HaqeqatElAmer
+//
+//  Created by Muhammad on 10/16/15.
+//  Copyright © 2015 Muhammad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
